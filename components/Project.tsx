722e6a9d8d8a702ed6458e95f79d9f2d0d4dfb49
Angelika Bauerová <angelika.bauerova@gmail.com>
2023-08-18 15:26:24 +0200

    responsivity of projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -12,37 +12,34 @@ export default function Project({
   githubUrl,
 }: ProjectProps) {
   return (
-    <div className="group mb-3 sm:mb-8 last:mb-0 ">
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last:mb-0 hover:bg-gray-200 transition group-even_pl-8">
-        <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
-          <h3 className="text-2xl semi-bold">{title}</h3>
-          <p className="mt-2 mb-4 leading-relaxed text-gray-700">
-            {description}
-          </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-          <a
-            href={githubUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-600 hover:text-gray-800"
-          >
-            <FaGithub className="text-4xl rounded-full mt-4" />
-          </a>
-        </div>
-        <Image
-          src={imageUrl}
-          alt="Project I worked on"
-          quality={95}
-          className="absolute top-10 -right-40 w-[28.25rem] rounded-lg shadow-2xl
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last:mb-0 hover:bg-gray-200 transition group-even_pl-8 mb-4">
+      <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+        <h3 className="text-2xl semi-bold">{title}</h3>
+        <p className="mt-2 mb-4 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+        <a
+          href={githubUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-600 hover:text-gray-800"
+        >
+          <FaGithub className="text-4xl rounded-full mt-4" />
+        </a>
+      </div>
+      <Image
+        src={imageUrl}
+        alt="Project I worked on"
+        quality={95}
+        className="sm:absolute top-10 -right-40 w-[28.25rem] rounded-lg shadow-2xl 
         transition
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
@@ -55,8 +52,7 @@ export default function Project({
         group-even:group-hover:translate-y-3
         group-even:group-hover:rotate-2
         "
-        />
-      </section>
-    </div>
+      />
+    </section>
   );
 }
